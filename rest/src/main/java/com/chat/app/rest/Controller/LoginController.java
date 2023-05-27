e6235054c93672ef7dad0d7cec82845a838b7246
Kevin Martinez <kevinvr@hotmail.es>
2023-05-27 01:16:27 -0600

    Help

--- rest/src/main/java/com/chat/app/rest/Controller/LoginController.java
+++ rest/src/main/java/com/chat/app/rest/Controller/LoginController.java
@@ -4,6 +4,9 @@ import com.chat.app.rest.Model.User;
 import com.chat.app.rest.Repository.TodoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.HttpClientErrorException;
+
+import java.nio.file.AccessDeniedException;
 
 @RestController
 @RequestMapping//("/Login")
@@ -16,18 +19,26 @@ public class LoginController {
         this.todoRepository = todoRepository;
     }
 
+    @PostMapping(value = "/register")
+    @ResponseBody
+    public User register(@RequestBody User user){
+        User created = this.todoRepository.save(user);
+        created.setPassword(null);
+        return created;
+    }
+
     @PostMapping(value = "/login")
-    public String saveLogin(@RequestBody User user){
+    @ResponseBody
+    public User saveLogin(@RequestBody User user){
         String email = user.getEmail();
         String password = user.getPassword();
 
         User storedUser = todoRepository.findByEmail(email);
 
         if (storedUser != null && storedUser.getPassword().equals(password)){
-            return "Succesful Login " + storedUser.getName();
-        } else {
-            return "Invalid!";
+            return storedUser;
         }
+        throw new Unau;
     }
 
 
