c81cbad9aa2fdde0b9f7ad60d614793a5043a74b
Fairbrook <kevinvr@hotmail.es>
2023-05-27 23:55:38 -0600

    Front url enviroment variable

--- rest/src/main/java/com/chat/app/rest/RestfulApiApplication.java
+++ rest/src/main/java/com/chat/app/rest/RestfulApiApplication.java
@@ -1,13 +1,16 @@
 package com.chat.app.rest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class RestfulApiApplication {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestfulApiApplication.class, args);
@@ -16,9 +19,12 @@ public class RestfulApiApplication {
 	@Bean
 	public WebMvcConfigurer corsConfigurer() {
 		return new WebMvcConfigurer() {
+			@Autowired
+			Environment environment;
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowCredentials(true).allowedOrigins("http://localhost:5173");
+				String frontUrl = this.environment.getProperty("custom.front-url");
+				registry.addMapping("/**").allowCredentials(true).allowedOrigins(frontUrl);
 			}
 		};
 	}
